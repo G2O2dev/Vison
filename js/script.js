7ca9a578d972e3eb8e0f762f540b50c0b02b2e5a
GOLJIE <g2o2mc@yandex.com>
2022-08-17 00:23:05 +0300

    Fixed an issue where the start section was too long on some mobile browsers

--- js/script.js
+++ js/script.js
@@ -47,6 +47,11 @@ function redirectFocus(){
     document.getElementById("msg-txt").focus();
 }
 
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 if(!navigator.userAgent.toLowerCase().indexOf('firefox') > -1 && window.getComputedStyle(document.querySelector('.work')).overflow != "visible"){
     var s;
     s = document.createElement('script');
