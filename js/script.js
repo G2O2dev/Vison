403495b1b4cf6a8cc8195b47d09ebd0de73a1394
GOLJIE <g2o2mc@yandex.com>
2022-08-22 12:38:17 +0300

    Added modal windows

--- js/script.js
+++ js/script.js
@@ -44,9 +44,78 @@ function redirectFocus(){
     document.getElementById("msg-txt").focus();
 }
 
-if(!navigator.userAgent.toLowerCase().indexOf('firefox') > -1 && window.getComputedStyle(document.querySelector('.work')).overflow != "visible"){
-    var s;
-    s = document.createElement('script');
-    s.src = "js/SmoothScroll.js";
+function getEl(Id){
+    return document.getElementsByClassName(Id)[0];
+}
+
+var modalBg = getEl("modal-bg");
+var modals = getEl("modals");
+var animation = 0;
+
+function openModal(modalName){
+    if(animation) return;
+    animation = 1;
+
+    var modal = getEl(modalName);
+    document.body.style.overflow = 'hidden';
+
+    modalBg.classList.add("modal-bg-open");
+    modals.classList.add("show-modals");
+    modal.classList.add("modal-work--show");
+    modal.classList.add("ds-block");
+
+    setTimeout(function (){
+        modal.classList.add('visible');
+        animation = 0;
+    }, 1500)
+}
+function hideModal(){
+    if(event.target !== event.currentTarget) return;
+    if(animation) return;
+    animation = 1;
+
+    var modal;
+    var allModal = document.getElementsByClassName('modal-work');
+    for (let i =0; i < allModal.length; i++){
+        if (allModal[i].classList.contains('modal-work--show'))
+            modal = allModal[i];
+    }
+
+    modal.classList.remove("modal-work--show");
+    modal.style.animation = 'none';
+    modal.offsetHeight;
+    modal.classList.add("modal-work--hide");
+
+    modal.classList.add('hide-modal');
+    modal.classList.remove('show-modal');
+
+    setTimeout(function (){
+        modal.classList.remove('visible');
+        modalBg.classList.add("modal-bg-hide");
+    }, 1500)
+
+    setTimeout(function () {
+        modalBg.classList.remove("modal-bg-open");
+        modalBg.classList.remove("modal-bg-hide");
+
+        modals.classList.remove("show-modals");
+
+        modal.classList.remove("hide-modal");
+        modal.classList.remove("modal-work--hide");
+        modal.classList.remove("ds-block");
+        document.body.style.overflow = 'visible';
+        animation = 0;
+        },3000);
+}
+
+function loadScript(path){
+    var s = document.createElement('script');
+    s.src = path;
     document.head.appendChild(s);
 }
+
+if(!navigator.userAgent.toLowerCase().indexOf('firefox') > -1 && window.getComputedStyle(document.querySelector('.work')).overflow != "visible"){
+    loadScript("js/SmoothScroll.js");
+}
+
+
