fe33a13a72a333330f3211ceb20920bffe3a5ad9
Admin <goljiemc@yandex.omc>
2024-06-11 18:45:20 +0300

    Vite better image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,48 @@
-import {ViteMinifyPlugin} from "vite-plugin-minify";
-import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
+import {defineConfig} from "vite";
 
-export  default {
+import {ViteMinifyPlugin} from "vite-plugin-minify";
+
+import viteImagemin2 from "@vheemstra/vite-plugin-imagemin";
+import viteImagemin from "vite-plugin-imagemin";
+import imageminSvgo from "imagemin-svgo";
+import imageminAvifenc from "@vheemstra/imagemin-avifenc";
+import imageminMozjpeg from 'imagemin-mozjpeg'
+
+
+export  default defineConfig({
     root: 'src',
+    base: './',
     build: {
-        outDir: '../dist',
+        inject: true,
+
+        outDir: './dist',
         emptyOutDir: true,
 
         assetsInlineLimit: false,
         minify: 'esbuild',
         cssMinify: 'esbuild',
     },
-    css: {
-
-    },
     plugins: [
         // Inspect(),
         ViteMinifyPlugin({}),
-        ViteImageOptimizer({}),
+        viteImagemin({
+            plugins: {
+                svg: imageminSvgo(),
+            },
+        }),
+        viteImagemin2({
+            exclude: [/node_modules/, /og.png/],
+
+            plugins: {
+                jpg: imageminMozjpeg(),
+            },
+
+            makeAvif: {
+                formatFilePath: (file) => file,
+                plugins: {
+                    png: imageminAvifenc(),
+                },
+            },
+        }),
     ]
-}
+});
